77dddb4e637ce20ed13f7a91ef32546dd3b80fb3
ArTyy022 <toledoarthur11@gmail.com>
2025-07-26 13:11:40 -0300

    Add files via upload

---
+++ src/Estudos.java
@@ -0,0 +1,7 @@
+public class Estudos  {
+    public static void main(String[] args) {
+        System.out.println("Anotações de estudos:");
+        System.out.println("1. Revisar conceitos de Java.");
+        System.out.println("2. Praticar programação com exemplos.");
+    }
+}
